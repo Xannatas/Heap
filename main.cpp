d1365a3a0990b5a6bee01b22dbc6bdf0db81c656
banta-isaiah <bantaib@whitman.edu>
2017-11-25 00:55:20 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,7 @@
 /* Isaiah Banta Allison Collier */
 
 /* 
-ABOVE AND BEYOND: 
-	Impleneted a for loop that runs our program multiple times, opens a text file,
+	A for loop runs our program multiple times, opens a text file,
 	and inputs the size of our Data Set and the time it took, in milliseconds, for
 	our heap sort and c++ sort() algorithm to be implemented. These values are 
 	inputed in a CSV format so the data can then be imported into an Excel Spreadsheet
@@ -20,6 +19,8 @@ ABOVE AND BEYOND:
 using namespace std;
 
 int main() {
+	
+	//File management
  	ofstream sort1;
 	ofstream sort2;
  	ofstream sort1log;
@@ -28,21 +29,29 @@ int main() {
  	sort2.open("c++Sort.txt");
  	sort1log.open("heapSortLog.txt");
  	sort2log.open("c++SortLog.txt");
+	
+	//Initialize our needed ints
  	int initial, end, elapsed, initial_2, end_2, elapsed_2;
+	
+	//Time value
 	struct timeval tp;
+	
 	int dataSet = 0;
+	
+	//Run our sorting algorithm multiple times with multiple data sets
  	for(int test=10000;test<=250000;test+=10000) {
 	dataSet = test;
 	 
  	Heap heap(dataSet); 	// initialize Heap
  	int array[dataSet]; 	// allocate array memory
  	int array2[dataSet];
- 	srand(time(0));			// seed our random number generator 
+		
+ 	srand(time(0));		// seed our random number generator with current time
  	
  	for(int i=0;i<dataSet;i++) {
  		int p = rand() % 100;
-  		array[i] = p;
-		array2[i] = p;
+  		array[i] = p;	//Array to be inserted and sorted with our Heap
+		array2[i] = p;	//Array to be sorted with C++ sorting algorithm
 }
 
 // HEAP SORT BEGIN.. //
@@ -56,13 +65,12 @@ int main() {
   	end = tp.tv_sec*1000+tp.tv_usec/1000;
 // HEAP SORT END. //
 	
-	elapsed = end - initial;
-	
-    //for(int b=0;b<dataSet;b++) {cout<<array[b]<<endl;}
+	elapsed = end - initial;	//Total elapsed time.
 
 	cout << "Heap sort size: " << dataSet;
 	cout << " time: " << elapsed << " milliseconds" << endl;
 
+		
 // C++ ALGORITHM SORT BEGIN.. //
 	gettimeofday(&tp,NULL);
   	initial_2 = tp.tv_sec*1000+tp.tv_usec/1000;
@@ -73,21 +81,25 @@ int main() {
   	end_2 = tp.tv_sec*1000+tp.tv_usec/1000;
 // C++ ALGORITHM SORT END. //
 	
-	elapsed_2 = end_2 - initial_2;
+	elapsed_2 = end_2 - initial_2;	//Total elapsed time.
 
 	cout << "sort() size:  " << dataSet;
 	cout << " time: : " << elapsed_2 << " milliseconds" << endl;
 	
+	//Insert relevant data points into CSV txt files.
 	sort1 << dataSet <<","<< elapsed<<endl;
         sort1log << log2(dataSet) << "," << log2(elapsed) <<endl; 
         sort2 << dataSet << "," << elapsed_2<<endl;
 	sort2log << log2(dataSet) << "," << log2(elapsed_2) <<endl;
 
 }
+	//File management
  	sort1.close();
  	sort1log.close();
  	sort2.close();
  	sort2log.close();
+	
+//End of program
 return 0;
 }
 
